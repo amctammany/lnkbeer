1a33bf1ffb0d9fd55c2981fdd416b0ecd83d315a
Alex McTammany <apmctammany@gmail.com>
2024-11-28 09:26:06 -0500

    good enough

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -19,7 +19,7 @@ main()
     await prisma.$disconnect();
   })
   .catch(async (e) => {
-    // eslint-disable-next-line
+     
     console.error(e);
     await prisma.$disconnect();
     process.exit(1);
