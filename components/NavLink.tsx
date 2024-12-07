3df79f2a129e668aa835e9277bca38b1bd5710f7
Alex McTammany <apmctammany@gmail.com>
2024-12-07 02:20:22 -0500

    fuck sidebar

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,15 +1,20 @@
 "use client";
 import Link, { LinkProps } from "next/link";
 import { useSidebar } from "./ui/sidebar";
+import { useRouter } from "next/navigation";
 
 export type NavLinkProps = LinkProps & {
   href: string;
   children?: React.ReactNode;
 };
 export const NavLink = ({ href, children, ...props }: NavLinkProps) => {
-  const { isMobile, openMobile, toggleSidebar } = useSidebar();
+  const { isMobile, openMobile, setOpenMobile, toggleSidebar } = useSidebar();
+  const { replace } = useRouter();
   const handleClick: React.MouseEventHandler<HTMLAnchorElement> = (e) => {
-    if (isMobile && openMobile) toggleSidebar();
+    if (isMobile && openMobile) setOpenMobile(false);
+    replace(e.currentTarget.href);
+    e.preventDefault();
+    return false;
   };
   return (
     <Link href={href} onClick={handleClick} {...props}>
