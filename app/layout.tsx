7201a12b631f4b5a6f8d9b86e7a1a4d5a6d4789e
Alex McTammany <apmctammany@gmail.com>
2024-12-07 02:47:58 -0500

    good

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SidebarProvider defaultOpen={false}>
+        <SidebarProvider>
           <AppSidebar />
           <SidebarInset>{children}</SidebarInset>
         </SidebarProvider>
