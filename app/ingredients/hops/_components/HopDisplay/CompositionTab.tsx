f1b513c675a97152da0ebfc2a97077837c50a801
Alex McTammany <apmctammany@gmail.com>
2024-12-07 04:05:25 -0500

    fixed ranges

--- app/ingredients/hops/_components/HopDisplay/CompositionTab.tsx
+++ app/ingredients/hops/_components/HopDisplay/CompositionTab.tsx
@@ -2,6 +2,7 @@ import { Range } from "@/components/Range";
 import { HopInput } from "@/types/ingredient";
 type RangeProp = {
   name: keyof HopInput;
+  label?: string;
   min: number;
   max: number;
   range: [number, number];
@@ -10,38 +11,44 @@ type RangeProp = {
 const rangeProps: (hop: HopInput) => RangeProp[] = (hop) => [
   {
     name: "alphaRange",
+    label: "Alpha",
     min: 0,
-    max: 50,
+    max: 30,
     range: [hop.alphaLow!, hop.alphaHigh!],
   },
   {
     name: "betaRange",
+    label: "Beta",
     min: 0,
-    max: 50,
+    max: 25,
     range: [hop.betaLow!, hop.betaHigh!],
   },
   {
     name: "caryophylleneRange",
+    label: "Caryophyllene",
     min: 0,
-    max: 50,
+    max: 20,
     range: [hop.caryophylleneLow!, hop.caryophylleneHigh!],
   },
   {
     name: "cohumuloneRange",
+    label: "Cohumulone",
     min: 0,
-    max: 50,
+    max: 70,
     range: [hop.cohumuloneLow!, hop.cohumuloneHigh!],
   },
   {
     name: "myrceneRange",
+    label: "Myrcene",
     min: 0,
-    max: 50,
+    max: 80,
     range: [hop.myrceneLow!, hop.myrceneHigh!],
   },
   {
     name: "totalOilRange",
+    label: "Total Oils",
     min: 0,
-    max: 50,
+    max: 8,
     range: [hop.totalOilLow!, hop.totalOilHigh!],
   },
 ];
@@ -51,7 +58,7 @@ export type CompositionTabProps = {
 };
 export function CompositionTab({ src }: CompositionTabProps) {
   return (
-    <div className="">
+    <div className="m-2">
       {rangeProps(src).map((props) => (
         <Range key={props.name} label={props.name} {...props} />
       ))}
