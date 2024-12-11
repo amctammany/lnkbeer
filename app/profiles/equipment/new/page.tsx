8308a9777769a1e21c2e72f2c17bbdfd49b698cc
Alex McTammany <apmctammany@gmail.com>
2024-12-10 22:17:02 -0500

    added same redirect fucntionality for all profile new pages

--- app/profiles/equipment/new/page.tsx
+++ app/profiles/equipment/new/page.tsx
@@ -1,30 +1,35 @@
 import { EquipmentProfileForm } from "@/app/profiles/equipment/_components/EquipmentProfileForm";
 import { getEquipmentProfile } from "@/app/profiles/equipment/queries";
 import { updateEquipmentProfile } from "@/app/profiles/equipment/actions";
-type EquipmentProfileCreatorPageProps = {
-  params: Promise<{
-    slug: string;
-  }>;
-};
+import { prisma } from "@/lib/client";
+import { auth } from "@/app/auth";
+import slugify from "slugify";
+import { redirect } from "next/navigation";
+type EquipmentProfileCreatorPageProps = any;
 
-export async function generateMetadata({
-  params,
-}: EquipmentProfileCreatorPageProps) {
-  const { slug } = await params;
+export async function generateMetadata({}: EquipmentProfileCreatorPageProps) {
   return {
-    title: `LNK EquipmentProfile: ${slug}`,
+    title: `LNK EquipmentProfile Creator`,
   };
 }
 
-export default async function EquipmentProfileCreatorPage({
-  params,
-}: EquipmentProfileCreatorPageProps) {
-  const { slug } = await params;
-  const equipmentProfile = await getEquipmentProfile(slug);
-  return (
-    <EquipmentProfileForm
-      src={equipmentProfile}
-      action={updateEquipmentProfile}
-    />
-  );
+export default async function EquipmentProfileCreatorPage({}: EquipmentProfileCreatorPageProps) {
+  const session = await auth();
+  if (!session?.user)
+    return redirect("/admin/login?returnUrl=/profiles/mash/new");
+  const user = await prisma.user.findFirst({
+    where: { id: session?.user?.id },
+    include: {
+      equipmentProfiles: { select: { id: true, name: true } },
+    },
+  });
+  if (!user) throw new Error("Invalid User?");
+  const name = `${user?.name} - EquipmentProfile ${user?.equipmentProfiles.length}`;
+  const res = await prisma.equipmentProfile.create({
+    data: {
+      name,
+      slug: slugify(name, { lower: true }),
+    },
+  });
+  redirect(`/profiles/mash/${res.slug}/edit`);
 }
--- app/profiles/water/new/page.tsx
+++ app/profiles/water/new/page.tsx
@@ -1,6 +1,10 @@
 import { WaterProfileForm } from "@/app/profiles/water/_components/WaterProfileForm";
 import { getWaterProfile } from "@/app/profiles/water/queries";
 import { updateWaterProfile } from "@/app/profiles/water/actions";
+import { auth } from "@/app/auth";
+import { redirect } from "next/navigation";
+import { prisma } from "@/lib/client";
+import slugify from "slugify";
 type WaterProfileCreatorPageProps = {
   params: Promise<{
     slug: string;
@@ -16,10 +20,23 @@ export async function generateMetadata({
   };
 }
 
-export default async function WaterProfileCreatorPage({
-  params,
-}: WaterProfileCreatorPageProps) {
-  const { slug } = await params;
-  const waterProfile = await getWaterProfile(slug);
-  return <WaterProfileForm src={waterProfile} action={updateWaterProfile} />;
+export default async function WaterProfileCreatorPage({}: WaterProfileCreatorPageProps) {
+  const session = await auth();
+  if (!session?.user)
+    return redirect("/admin/login?returnUrl=/profiles/water/new");
+  const user = await prisma.user.findFirst({
+    where: { id: session?.user?.id },
+    include: {
+      waterProfiles: { select: { id: true, name: true } },
+    },
+  });
+  if (!user) throw new Error("Invalid User?");
+  const name = `${user?.name} - WaterProfile ${user?.waterProfiles.length}`;
+  const res = await prisma.waterProfile.create({
+    data: {
+      name,
+      slug: slugify(name, { lower: true }),
+    },
+  });
+  redirect(`/profiles/water/${res.slug}/edit`);
 }
