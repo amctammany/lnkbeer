2a4306eda88b71b251bb4189f669f0af510c4873
Alex McTammany <apmctammany@gmail.com>
2024-12-12 14:58:06 -0500

    loading

---
+++ app/ingredients/loading.tsx
@@ -0,0 +1,9 @@
+import { AppBarLayout } from "@/components/AppBarLayout";
+
+export default function Loading() {
+  return (
+    <AppBarLayout title="Ingredients">
+      <div>Loading Ingredients...</div>
+    </AppBarLayout>
+  );
+}
---
+++ app/styles/loading.tsx
@@ -0,0 +1,9 @@
+import { AppBarLayout } from "@/components/AppBarLayout";
+
+export default function Loading() {
+  return (
+    <AppBarLayout title="Styles">
+      <div>Loading Styles...</div>
+    </AppBarLayout>
+  );
+}
