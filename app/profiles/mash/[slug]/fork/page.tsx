399e42c3baa11a711b67abaf67f67e49236c32de
Alex McTammany <apmctammany@gmail.com>
2024-12-12 01:02:59 -0500

    good enough

--- app/profiles/mash/[slug]/fork/page.tsx
+++ app/profiles/mash/[slug]/fork/page.tsx
@@ -1,6 +1,10 @@
+import { auth } from "@/app/auth";
 import { MashProfileForm } from "../../_components/MashProfileForm";
-import { updateMashProfile } from "../../actions";
+import { createMashProfile, updateMashProfile } from "../../actions";
 import { getMashProfile } from "../../queries";
+import slugify from "slugify";
+import { prisma } from "@/lib/client";
+import { ExtendedMashStep } from "@/types/Profile";
 
 export type MashProfileForkPageProps = {
   params: Promise<{ slug: string }>;
@@ -9,8 +13,33 @@ export default async function MashProfileForkPage({
   params,
 }: MashProfileForkPageProps) {
   const { slug } = await params;
-  const old = await getMashProfile(slug);
-  const fork = { ...old, forkedFrom: old.id };
+  const session = await auth();
+  if (!session?.user) {
+    throw new Error("Unauthorized User");
+  }
+  const { id, owner, origin, steps, ...old } = await getMashProfile(slug);
+  const count = await prisma.mashProfile.count({
+    where: { userId: session.user.id, forkedFrom: id },
+  });
+  const name = `${session.user.name} - ${old.name} (${count})`;
 
+  const fork = await prisma.mashProfile.create({
+    data: {
+      ...old,
+      name,
+      slug: slugify(name, { lower: true }),
+      forkedFrom: id,
+      steps: {
+        createMany: {
+          data: (steps as ExtendedMashStep[]).map(
+            ({ id, MashProfile, mashProfileId, ...s }) => s,
+          ),
+        },
+      },
+    },
+    include: {
+      steps: true,
+    },
+  });
   return <MashProfileForm src={fork} action={updateMashProfile} />;
 }
