fd1a71ea234e0539f5a714830299dea7e4ccf2e6
Alex McTammany <apmctammany@gmail.com>
2025-03-03 01:56:20 -0500

    rdio?

--- app/ingredients/hops/_components/HopDisplay/NotesTabForm.tsx
+++ app/ingredients/hops/_components/HopDisplay/NotesTabForm.tsx
@@ -2,28 +2,53 @@
 import { Form } from "@/components/Form/Form";
 import { TextArea } from "@/components/Form/TextArea";
 import { Button } from "@/components/ui/button";
+import { Label } from "@/components/Form/Label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { useActionForm } from "@/hooks/useActionForm";
 import { HopNote } from "@prisma/client";
+import { UseFormRegisterReturn } from "react-hook-form";
 type HopNoteInput = Partial<HopNote> & {
   id?: any;
+  flavor?: number;
 };
 export type NotesTabFormProps = {
   src?: HopNote;
   //user?: any;
   action?: any;
 };
+const RangeSelect = (props: UseFormRegisterReturn) => {
+  console.log(props);
+  return (
+    <RadioGroup defaultValue="option-one" className="flex">
+      {Array.from({ length: 10 }).map((_, i) => (
+        <div key={i} className="flex items-center space-y-0 space-x-2 ">
+          <label htmlFor={`opt-${i}`}>
+            <b>{i}</b>
+          </label>
+          <input id={`opt-${i}`} {...props} type="radio" value={i} />
+        </div>
+      ))}
+    </RadioGroup>
+  );
+};
 export function NotesTabForm({ action, src }: NotesTabFormProps) {
-  console.log(src);
   const { state, register, control, getValues, formAction } =
     useActionForm<HopNoteInput>(action, src!);
 
-  console.log(formAction);
   return (
     <Form action={formAction}>
-      <input type="hidden" {...register("userEmail")} />
-      <input type="hidden" {...register("hopId")} />
-      <TextArea {...register("comments")} />
-      <input type="submit" />
+      <div className="grid w-full">
+        <input type="hidden" {...register("userEmail")} />
+        <input type="hidden" {...register("hopId")} />
+        <div>
+          <TextArea {...register("comments")} />
+        </div>
+        <div>
+          <RangeSelect {...register("flavor")} />
+        </div>
+
+        <input type="submit" />
+      </div>
     </Form>
   );
 }
